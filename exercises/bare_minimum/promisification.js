38ac98a7da9e907028989a4f9543fa2c33143d0e
Hack Reactor Student <student@hackreactor.com>
2017-06-10 16:16:20 -0700

    Implement promisification.js async functions

--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -27,7 +27,7 @@ var getGitHubProfile = function(user, callback) {
   });
 };
 
-var getGitHubProfileAsync; // TODO
+var getGitHubProfileAsync = Promise.promisify(getGitHubProfile);
 
 
 // (2) Asyncronous token generation
@@ -38,25 +38,43 @@ var generateRandomToken = function(callback) {
   });
 };
 
-var generateRandomTokenAsync; // TODO
+var generateRandomTokenAsync = Promise.promisify(generateRandomToken);
 
 
 // (3) Asyncronous file manipulation
 var readFileAndMakeItFunny = function(filePath, callback) {
   fs.readFile(filePath, 'utf8', function(err, file) {
-    if (err) { return callback(err); }
-   
+    if (err) { return callback(err, null); }
     var funnyFile = file.split('\n')
       .map(function(line) {
         return line + ' lol';
       })
       .join('\n');
-
     callback(funnyFile);
   });
 };
 
-var readFileAndMakeItFunnyAsync; // TODO
+var readFileAndMakeItFunnyAsync = function(filePath) {
+  var promise = new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', function(err, content) {
+      if (err) {
+        reject(err);
+      } else {
+        var funnyFile = content.split('\n')
+          .map(function(line) {
+            return line + ' lol';
+          })
+          .join('\n');
+
+        resolve(funnyFile);
+      }
+    });
+  });
+
+  return promise;
+};
+
+
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
